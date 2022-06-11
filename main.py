154f68008a5129570ed004c901674516254b2140
Паркин Максим <carrotypirate@mail.ru>
2022-06-11 18:24:30 +0500

    без 27

--- main.py
+++ main.py
@@ -60,7 +60,58 @@ def z15():
             OK *= ((x % 3 == 0) <= (not (x % 5 == 0))) or (x + A >= 70)
         if OK:
             print(A)
+def z17():
+    with open('z17.txt') as f:
+        n = [int(x) for x in f]
+    mn = min(x for x in n if x % 17 == 0)
+    sums = []
+    for a, b in zip(n, n[1:]):
+        if a % mn == 0 or b% mn == 0:
+            sums.append(a+b)
+    print((len(sums), max(sums)))
 
+
+def z22():
+    for x in range(10000000):
+        s = x
+        P = 10
+        Q = 8
+        K1 = 0
+        K2 = 0
+        while s <= 100:
+            s = s + P
+            K1 = K1 + 1
+        while s >= Q:
+            s = s - Q
+            K2 = K2 + 1
+        K1 += s
+        K2 += s
+        if K1 == 10 and K2 == 19:
+            print(x)
+            break
+
+
+def z24():
+    f = open('z24.txt')
+    n = f.read()
+    n = n.replace('AB', 'x')
+    n = n.replace('AC', 'x')
+    k=0
+    m=0
+    for i in range(len(n)):
+        if n[i]=='x':
+            k+=1
+            m = max(m,k)
+        else:
+            k=0
+    print(m)
+
+def z25():
+    for a in range(10):
+        for b in range(10):
+            x =  int(f'12345{a}6{b}8')
+            if x % 17 == 0:
+                print(x,x//17)
 
 if __name__ == '__main__':
-    z15()
+    z25()
